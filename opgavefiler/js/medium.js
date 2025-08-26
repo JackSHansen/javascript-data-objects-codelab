c68af0cebf3d7ffd54b939712c2e838aa63b47ae
JackVS1 <jsh1999@gmail.com>
2025-08-26 13:35:22 +0200

    Medium ompaver done
    lavede medium opgaverne

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -1,7 +1,13 @@
 /* Opgave 1*/
 console.group("opgave 1");
 
+const data = {
+  items: ["æble", "banan", "pære", "appelsin", "mango"]
+};
 
+data.items.push("jordbær");
+
+console.log(data);
 
 console.groupEnd();
 
@@ -10,13 +16,25 @@ console.groupEnd();
 /*opgave 2*/
 console.group("opgave 2");
 
+const data = {
+  items: ["æble", "banan", "pære", "appelsin", "mango"]
+};
 
+data.items.splice(1, 1);
 
 console.groupEnd();
 
 
 /*opgave 3*/
 console.group("opgave 3");
+
+const data = {
+  items: ["æble", "banan", "pære", "appelsin", "mango"]
+};
+
+data.items[2] = "changed";
+
+console.log(data);
 
 console.groupEnd();
 
